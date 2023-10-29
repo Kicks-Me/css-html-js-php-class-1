b678139bf8d4f0a1ed9b11ca7b81f3915ba70efa
Kicks Me <45519441+Kicks-Me@users.noreply.github.com>
2023-10-29 21:42:11 +0700

    202310292142

--- js edit html/test.js
+++ js edit html/test.js
@@ -3,3 +3,18 @@ setInterval(() => {
         document.getElementById('content').classList.toggle('bg');
 }, 1000);
 
+function showhidemenu(){
+        if(document.getElementById('menu'))
+        {
+                // if(document.getElementById('menu').style.display == 'block')
+                // {
+                //         document.getElementById('menu').style.display = 'none';
+                // }
+                // else
+                // {
+                //         document.getElementById('menu').style.display = 'block';
+                // }
+
+                document.getElementById('menu').classList.toggle('menu');
+        }
+}
